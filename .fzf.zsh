501ec3da2d2a87a161e6b4fdff1727681e3cfa1d
sysop <sysop@mail.hypokrites.me>
2014-04-03 18:40:07 +0000

    removed user name from path to fzf.bash/zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -2,13 +2,13 @@
 # ------------------
 unalias fzf 2> /dev/null
 fzf() {
-  /usr/sbin/ruby /home/ciaran/.fzf/fzf "$@"
+  /usr/sbin/ruby ~/.fzf/fzf "$@"
 }
 export -f fzf > /dev/null
 
 # Auto-completion
 # ---------------
-[[ $- =~ i ]] && source /home/ciaran/.fzf/fzf-completion.zsh
+[[ $- =~ i ]] && source ~/.fzf/fzf-completion.zsh
 
 # Key bindings
 # ------------
